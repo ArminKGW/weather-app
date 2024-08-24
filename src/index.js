4a6b0e0536ba5bca602e9d2a55927d638e695d83
Armin <160379151+ArminKGW@users.noreply.github.com>
2024-08-24 14:57:31 +0200

    Add button to switch between °C and °F

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 import './styles.css';
 
-const btn = document.querySelector('button');
+const searchBtn = document.querySelector('form > button');
+const switchBtn = document.querySelector('.switch');
 const inputCity = document.querySelector('input');
+const box = document.querySelector('.weather-data');
 const city = document.querySelector('.city');
 const temp = document.querySelector('.temp');
 const feelsLike = document.querySelector('.feels-like');
 const humidity = document.querySelector('.humidity');
 const windSpeed = document.querySelector('.wind-speed');
+let mode = 'C';
 
 function showError(error) {
   city.textContent = error;
@@ -14,6 +17,45 @@ function showError(error) {
   feelsLike.textContent = '';
   humidity.textContent = '';
   windSpeed.textContent = '';
+}
+
+function getTemp(tempElement, feelsLikeElement) {
+  let currentTemp, currentFeelsLike;
+  if (mode === 'C') {
+    currentTemp = tempElement.textContent.replace('°C', '');
+    currentFeelsLike = parseInt(
+      feelsLikeElement.textContent.match(/(\d+)/)[0],
+      10,
+    );
+    return { currentTemp, currentFeelsLike };
+  } else if (mode === 'F') {
+    currentTemp = tempElement.textContent.replace('°F', '');
+    currentFeelsLike = parseInt(
+      feelsLikeElement.textContent.match(/(\d+)/)[0],
+      10,
+    );
+    return { currentTemp, currentFeelsLike };
+  }
+}
+
+function switchTemp() {
+  let newTemp, newFeelsLike;
+  let { currentTemp, currentFeelsLike } = getTemp(temp, feelsLike);
+  if (mode === 'C') {
+    newTemp = Math.round(Number(currentTemp) * 1.8 + 32);
+    newFeelsLike = Math.round(Number(currentFeelsLike) * 1.8 + 32);
+    temp.textContent = `${newTemp} °F`;
+    feelsLike.textContent = `Feels like: ${newFeelsLike} °F`;
+    mode = 'F';
+    return;
+  } else if (mode === 'F') {
+    newTemp = Math.round((Number(currentTemp) - 32) / 1.8);
+    newFeelsLike = Math.round((Number(currentFeelsLike) - 32) / 1.8);
+    temp.textContent = `${newTemp} °C`;
+    feelsLike.textContent = `Feels like: ${newFeelsLike} °C`;
+    mode = 'C';
+    return;
+  }
 }
 
 async function getData(city) {
@@ -27,12 +69,22 @@ async function getData(city) {
       throw new Error(`No results found. status: ${response.status}`);
     }
     const data = await response.json();
+    console.log(data);
     if (!data || !data.currentConditions) {
       throw new Error(`No weather data available for ${city}.`);
     }
     const currentConditions = data.currentConditions;
-    const { temp, feelslike, humidity, windspeed } = currentConditions;
-    return { temp, feelslike, humidity, windspeed };
+    const address = data.resolvedAddress;
+    let { temp, feelslike, humidity, windspeed } = currentConditions;
+    let roundTemp, roundFeelsLike;
+    if (mode === 'F') {
+      roundTemp = Math.round(temp * 1.8 + 32);
+      roundFeelsLike = Math.round(feelslike * 1.8 + 32);
+    } else {
+      roundTemp = Math.round(temp);
+      roundFeelsLike = Math.round(feelslike);
+    }
+    return { roundTemp, roundFeelsLike, humidity, windspeed, address };
   } catch (error) {
     showError(error.message);
   }
@@ -49,17 +101,28 @@ async function showData() {
     inputCity.value[0].toUpperCase() + inputCity.value.slice(1).toLowerCase();
   const data = await getData(cityName);
   if (!data) return;
-  city.textContent = cityName;
-  temp.textContent = `${data.temp} °C`;
-  feelsLike.textContent = `Feels like: ${data.feelslike} °C`;
+  const cityNameFinal = data.address.split(',')[0].trim();
+  city.textContent = cityNameFinal;
+  if (mode === 'C') {
+    temp.textContent = `${data.roundTemp} °C`;
+    feelsLike.textContent = `Feels like: ${data.roundFeelsLike} °C`;
+  } else if (mode === 'F') {
+    temp.textContent = `${data.roundTemp} °F`;
+    feelsLike.textContent = `Feels like: ${data.roundFeelsLike} °F`;
+  }
   humidity.textContent = `Humidity: ${data.humidity} %`;
   windSpeed.textContent = `Wind Speed: ${data.windspeed} km/h`;
+  box.classList.remove('hidden');
 }
 
 function handleEvents() {
-  btn.addEventListener('click', (e) => {
+  searchBtn.addEventListener('click', (e) => {
     e.preventDefault();
     showData();
+  });
+
+  switchBtn.addEventListener('click', () => {
+    switchTemp();
   });
 }
 
